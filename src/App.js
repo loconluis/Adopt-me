a10cd9fdabb7434545d7432351a79a1e494c481b
Luis Locon <luis.locon@gmail.com>
2021-08-23 22:53:51 -0600

    setting up workflow

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-const Pet = (props) => {
-  return React.createElement('div', null, [
-    React.createElement('h2', {}, props.name),
-    React.createElement('h3', {}, props.animal),
-    React.createElement('h3', {}, props.breed),
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', { id: 'my-brand' }, 'Adopt Me!'),
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
     React.createElement(Pet, {
-      name: 'Luna',
-      animal: 'Dog',
-      breed: 'Havanese',
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
     }),
     React.createElement(Pet, {
-      name: 'Pepper',
-      animal: 'Bird',
-      breed: 'Cocktiel',
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cocktiel",
     }),
     React.createElement(Pet, {
-      name: 'Sudo',
-      animal: 'Dog',
-      breed: 'Wheaten Terrier',
+      name: "Sudo",
+      animal: "Dog",
+      breed: "Wheaten Terrier",
     }),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
