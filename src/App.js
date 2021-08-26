7c90b2954780584b8785d32e4f26bbba13d03b71
Luis Locon <luis.locon@gmail.com>
2021-08-25 23:21:22 -0600

    Adding Context API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState("darkblue");
+
   return (
     <StrictMode>
-      <div>
-        <Router>
-          <header>
-            <Link to="/">
-              <h1>Adopt Me!</h1>
-            </Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id" component={Details} />
-            <Route path="/" component={SearchParams} />
-          </Switch>
-        </Router>
-      </div>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <Router>
+            <header>
+              <Link to="/">
+                <h1>Adopt Me!</h1>
+              </Link>
+            </header>
+            <Switch>
+              <Route path="/details/:id" component={Details} />
+              <Route path="/" component={SearchParams} />
+            </Switch>
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </StrictMode>
   );
 };
--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,10 +1,11 @@
 // mostly took this from the React Docs
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
   state = {
     hasError: false,
+    redirect: false,
   };
 
   static getDerivedStateFromError() {
@@ -14,18 +15,24 @@ class ErrorBoundary extends Component {
   componentDidCatch(error, info) {
     // I log this to Senty, Azure Monitor, New Relic, TrackJS, etc.
     console.error("ErrorBoundary caught an error", error, info);
+    setTimeout(() => {
+      this.setState({ redirect: true });
+    }, 5000);
   }
 
   render() {
-    if (this.state.hasError) {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    } else if (this.state.hasError) {
       return (
         <h2>
           This listing has an error <Link to="/">Click here</Link> to go back to
-          the the home page.
+          the the home page or wait five seconds.
         </h2>
       );
+    } else {
+      return this.props.children;
     }
-    return this.props.children;
   }
 }
 
