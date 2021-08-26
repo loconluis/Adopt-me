02d44bf16e98d457fb86e337dad02139aab969a0
Luis Locon <luis.locon@gmail.com>
2021-08-25 19:38:35 -0600

    Adding react-router, Details Page, and Carousel Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <StrictMode>
       <div>
-        <h1 id="my-brand">Adopt Me!</h1>
-        <SearchParams />
+        <Router>
+          <header>
+            <Link to="/">
+              <h1>Adopt Me!</h1>
+            </Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id" component={Details} />
+            <Route path="/" component={SearchParams} />
+          </Switch>
+        </Router>
       </div>
     </StrictMode>
   );
